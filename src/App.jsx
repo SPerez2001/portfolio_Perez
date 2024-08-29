7d0d5d6ec66342690cead386f24d20b634d5d4bb
SPerez2001 <s.perez.501627@gmail.com>
2024-08-29 15:28:51 +0800

    navbar commit 13th

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { navbar, Navbar } from '@material-tailwind/react'
  
-export default function App() {
-  return (
-    
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+
+
+export default App
