e68b4a77a8769ea442d06b1c94a98c90ed91948e
SPerez2001 <s.perez.501627@gmail.com>
2024-08-29 19:39:21 +0800

    navbar commit 64th

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,7 @@ function App() {
                 <div classname="container" id="Resume">
                     <h1> Resume</h1>
                     <img
-                        src={require('/img/resume.png')}
+                        src={"/img/resume.png"}
                         alt="Resume"
                         width={400}
                         height={600}
