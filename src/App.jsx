5e9287131a12ad468e876ce6bec79af1a00554aa
SPerez2001 <s.perez.501627@gmail.com>
2024-08-29 14:13:22 +0800

    navbar commit 8th

--- src/App.jsx
+++ src/App.jsx
@@ -148,13 +148,7 @@ export function StickyNavbar() {
         </MobileNav>
       </Navbar>
       <div className="mx-auto max-w-screen-md py-12">
-        <Card className="mb-12 overflow-hidden">
-          <img
-            alt="nature"
-            className="h-[32rem] w-full object-cover object-center"
-            src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
-          />
-        </Card>
+        
         <Typography variant="h2" color="blue-gray" className="mb-2">
           What is Material Tailwind
         </Typography>
